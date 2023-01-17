66c15030c346ba3c6fed5885d33c5eee8c21f3dd
Jeongmmin <seo88kr@hanmail.net>
2023-01-17 20:59:45 +0900

    Fix : svg to png

--- pages/boards/new/index.js
+++ pages/boards/new/index.js
@@ -205,40 +205,12 @@ export default function index() {
               <p>Upload</p>
             </Photo>
             <Photo>
-              <PlusIcon />
-              {/* <svg
-                width='14'
-                height='14'
-                viewBox='0 0 14 14'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z' fill='#4F4F4F' />
-              </svg> */}
               <p>Upload</p>
             </Photo>
             <Photo>
-              {/* <svg
-                width='14'
-                height='14'
-                viewBox='0 0 14 14'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z' fill='#4F4F4F' />
-              </svg> */}
               <p>Upload</p>
             </Photo>
             <Photo>
-              {/* <svg
-                width='14'
-                height='14'
-                viewBox='0 0 14 14'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z' fill='#4F4F4F' />
-              </svg> */}
               <p>Upload</p>
             </Photo>
           </PhotoWrapper>
